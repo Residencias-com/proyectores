62356ded26c805d84ea80611d63b91853d076494
Eloy <eloymendzctz@gmail.com>
2019-10-29 11:02:24 -0600

    Templates para header,aside y footer

---
+++ application/views/templates/admin_all.php
@@ -0,0 +1,109 @@
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-primary navbar-dark">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="<?php echo base_url();?>/assets/#"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+<!-- Aqui va la minuatura xD -->
+<ul class="navbar-nav ml-auto">
+<li class="nav-item dropdown user-menu">
+  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+    <img src="<?php echo base_url();?>/img/admin.png" class="user-image img-circle" alt="User Image">
+    <span class="d-none d-md-inline">Administrador</span>
+  </a>
+  <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <!-- User image -->
+    <li class="user-header bg-primary">
+      <img src="<?php echo base_url();?>/img/admin.png" class="img-circle" alt="User Image">
+
+      <p>
+        Administrador
+        <small>Ing. Felipe de Jesus Niño de la Cruz</small>
+      </p>
+    </li>
+    <!-- Menu Body -->
+    <!-- Menu Footer-->
+    <li class="user-footer">
+      <a href="#" class="btn btn-default btn-flat">Editar</a>
+      <a href="#" class="btn btn-default btn-flat float-right">Cerrar sesión</a>
+    </li>
+  </ul>
+</li>
+</ul>
+    </nav>
+  <!-- /.navbar -->
+
+  <aside class="main-sidebar sidebar-dark-blue elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+      <img src="<?php echo base_url();?>/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-dark">ITTUX</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="" class="nav-link active">
+              <i class="nav-icon fas fa-tools"></i>
+              <p>
+                Menu
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="<?php echo base_url();?>admin" class="nav-link">
+                  <i class="far fa-comments nav-icon"></i>
+                  <p>Inicio</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="<?php echo base_url();?>admin/crear" class="nav-link">
+                  <i class="far fa-plus-square nav-icon"></i>
+                  <p>Insertar proyector</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="<?php echo base_url();?>admin/listar" class="nav-link">
+                  <i class="far fa-list-alt nav-icon"></i>
+                  <p>Listar proyectores</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="<?php echo base_url();?>admin/listar2" class="nav-link">
+                  <i class="far fa-user nav-icon"></i>
+                  <p>Listar usuario</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-file nav-icon"></i>
+                  <p>Generar reportes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-chart-bar nav-icon"></i>
+                  <p>Mostrar gráficas</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  </div>
---
+++ application/views/templates/footer_admin.php
@@ -0,0 +1,77 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="<?php echo base_url();?>/assets/http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.0-rc.1
+    </div>
+</footer>
+<!-- cierre wrapper -->
+</div>
+
+
+<!-- jQuery -->
+<script src="<?php echo base_url();?>/assets/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url();?>/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url();?>/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url();?>/assets/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url();?>/assets/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo base_url();?>/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?php echo base_url();?>/assets/plugins/jqvmap/maps/jquery.vmap.world.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url();?>/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url();?>/assets/plugins/moment/moment.min.js"></script>
+<script src="<?php echo base_url();?>/assets/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?php echo base_url();?>/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?php echo base_url();?>/assets/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo base_url();?>/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>/assets/dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url();?>/assets/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>/assets/dist/js/demo.js"></script>
+
+<!-- Aqui pongo scripts -->
+<!-- DataTables -->
+<script src="<?php echo base_url();?>/assets/plugins/datatables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url();?>/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+
+<!-- page script -->
+<script>
+$(document).ready(function () {
+$('#example1').DataTable({
+  language: {
+      "lengthMenu": "Mostrar por pagina: _MENU_",
+      "zeroRecords": "No se encontraron resultados en su busqueda",
+      "searchPlaceholder": "Buscar registros:",
+      "info": "Mostrando registros de _START_ al _END_ de un total de  _TOTAL_ registros",
+      "infoEmpty": "No existen registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "search": "Buscar:",
+      "paginate": {
+          "first": "Primero",
+          "last": "Último",
+          "next": "Siguiente",
+          "previous": "Anterior"
+      },
+  }
+});
+$('.sidebar-menu').tree();
+})
+</script>
+</body>
+</html>
