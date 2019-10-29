6c54ec1bbe29770707414a8f4bf1d75a2b9fb68d
Eloy <eloymendzctz@gmail.com>
2019-10-29 12:10:53 -0600

    Se creo la función editar, dentro del modelo "M_admin"

--- application/models/M_admin.php
+++ application/models/M_admin.php
@@ -6,10 +6,27 @@ class M_admin extends CI_Model {
       parent::__construct();
       $this->load->database();
   }
+
+// Esta funcion inserta los datos del POST a la tabla proyectores
   public function insert(){
       $datos = $this->input->post();
       unset($datos['inserta_proyector']);
 
       $this->db->insert('proyectores',$datos);
   }
+
+// Esta funcion edita un  proyector, por medio de ID
+  public function editar($idproyector){
+
+    $data = array(
+      'clave' => $this->input->post('clave'),
+			'marca' => $this->input->post('marca'),
+			'modelo' => $this->input->post('modelo'),
+			'num_serie' => $this->input->post('num_serie'),
+			'depto' => $this->input->post('depto'),
+    );
+    $this->db->where('idproyector', $idproyector);
+    $this->db->update('proyectores', $data);
+  }
+
 }
