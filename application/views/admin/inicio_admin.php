e67b2fa578d1408145fa97e3e8846e2b77b617e3
Eloy <eloymendzctz@gmail.com>
2019-10-29 11:11:23 -0600

    Creación de la vista inicio

---
+++ application/views/admin/inicio_admin.php
@@ -0,0 +1,37 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Sistema de registro de proyectores</h1>
+
+        </div><!-- /.container-fluid -->
+      </div>
+  </section>
+  <!-- Aqui en contenido -->
+  <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title"><b>Bienvenido</b></h3>
+
+          <!-- <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
+          </div> -->
+        </div>
+        <div class="card-body">
+          Bienvenido al sistema de registro de proyectores xD
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+    </div>
