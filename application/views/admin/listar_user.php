0f2dc4f08629f53a88f1dac80b190f6c22013d94
Eloy <eloymendzctz@gmail.com>
2019-10-29 20:48:46 -0600

    Creación de la vista listar_user

---
+++ application/views/admin/listar_user.php
@@ -0,0 +1,55 @@
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Sistema de registro de proyectores</h1>
+
+        </div><!-- /.container-fluid -->
+      </div>
+  </section>
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title"><b>Title</b></h3>
+
+          <!-- <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
+          </div> -->
+        </div>
+        <div class="card-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Carrera</th>
+              <th>Email</th>
+              <th>Opciones</th>
+            </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Eloy Mendoza</td>
+                <td>Informatica</td>
+                <td>eloymendzctz@gmail.com</td>
+                <td><div class="btn-group">
+                  <a href="" class="btn btn-primary"><span class="far fa-edit"></span></a>
+                  <a href="" class="btn btn-danger"><span class="far fa-trash-alt"></span></a>
+                </div></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+    </div>
