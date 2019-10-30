3bf380ae09d3e57568c0bb10dcbbed6d8c40629f
Eloy <eloymendzctz@gmail.com>
2019-10-29 22:43:44 -0600

    Editando el controlador Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,8 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+
+  public function __construct(){
+    parent::__construct();
+    // $this->load->helper(array('getmenu','url'));
+    $this->load->library(array('form_validation', 'session'));
+    $this->load->helper(array('auth/login_rules'));
+    $this->load->model('Auth');
+  }
+
   public function index(){
 		$this->load->view('general/login');
 	}
-
+  
 }
