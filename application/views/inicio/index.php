3129d112327b190c71995b5d43e5204a4e844c14
Eloy <eloymendzctz@gmail.com>
2019-11-07 11:00:48 -0600

    Cambios menores

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,13 +11,12 @@
 	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/all.min.js"></script>
 </head>
-
 <body class="body">
 	<div class="card card-container">
 		<h3>Elija el departamento:</h3>
-		<a href="<?php echo base_url(); ?>inicio/computo"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/computo.png" alt=""></a>
+		<a href="<?php echo base_url(); ?>prestamo/computo"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/computo.png" alt=""></a>
 		<span>Computo</span>
-		<a href="<?php echo base_url(); ?>inicio/academico"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/academico.png" alt=""></a>
+		<a href="<?php echo base_url(); ?>prestamo/academico"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/academico.png" alt=""></a>
 		<span>Academico</span>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -26,5 +24,4 @@
 	<script src="<?php echo base_url('assets/js/jquery-3.4.1.min.map'); ?>"></script>
 	<script src="<?php echo base_url('assets/js/auth/login.js'); ?>"></script>
 </body>
-
 </html>
