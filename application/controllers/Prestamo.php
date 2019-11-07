8574126b30731337545bbc07e289f4e5af90b39d
Eloy <eloymendzctz@gmail.com>
2019-11-07 11:08:22 -0600

    Editando controlador Prestamo
    Se agrego la función construct

--- application/controllers/Prestamo.php
+++ application/controllers/Prestamo.php
@@ -2,5 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Prestamo extends CI_Controller {
-  
+  public function __construct(){
+    parent::__construct();
+    $this->load->database();
+    $this->load->model('M_admin');
+    $this->load->helper("form");
+  }
 }
