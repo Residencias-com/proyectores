24348e37137809ebecae932d4dc6a31a0d71f297
Eloy <eloymendzctz@gmail.com>
2019-11-07 09:29:09 -0600

    Creacion de la vista Index

---
+++ application/views/inicio/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style2.css">
+	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>/assets/js/all.min.js"></script>
+</head>
+
+<body>
+	<div class="card card-container">
+		<h3>Elija el departamento:</h3>
+		<a href="<?php echo base_url(); ?>/inicio/computo"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/computo.png" alt=""></a>
+		<span>Computo</span>
+		<a href="<?php echo base_url(); ?>/inicio/academico"><img class="profile-img-card" src="<?php echo base_url(); ?>/img/academico.png" alt=""></a>
+		<span>Academico</span>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="<?php echo base_url('assets/js/jQuery-3.4.1.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery-3.4.1.min.map'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/auth/login.js'); ?>"></script>
+</body>
+
+</html>
