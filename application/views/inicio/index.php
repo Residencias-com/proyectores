594281c521555c86ccc412fd1e53170a90e31f0f
Eloy <eloymendzctz@gmail.com>
2019-11-12 14:20:33 -0600

    Modificando titulo

--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>Inicio</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style2.css">
