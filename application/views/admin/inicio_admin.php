af8dbb0a3cbac73b622465c8ec628465142f5d2e
Eloy <eloymendzctz@gmail.com>
2019-11-12 11:54:24 -0600

    Cambios menores

--- application/views/admin/inicio_admin.php
+++ application/views/admin/inicio_admin.php
@@ -18,12 +18,10 @@
         <div class="card-header">
           <h3 class="card-title"><b>Bienvenido</b></h3>
 
-          <!-- <div class="card-tools">
+          <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
               <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-          </div> -->
+          </div>
         </div>
         <div class="card-body">
           Bienvenido al sistema de registro de proyectores xD
