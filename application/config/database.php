5d71b978064bf3fc210fb32df889843eb5dc1c39
Sr.Speedy <eloymendzctz@gmail.com>
2020-01-10 11:15:27 -0600

    Configuracion database Sybase

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['db2'] = array(
+	'dsn'	=> 'dblib:host=10.10.0.17:5000;port=;dbname=bdtec',
+	'hostname' => '',
+	'username' => 'consulta',
+	'password' => 'Ittux19#',
+	'database' => '',
+	'dbdriver' => 'pdo',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
